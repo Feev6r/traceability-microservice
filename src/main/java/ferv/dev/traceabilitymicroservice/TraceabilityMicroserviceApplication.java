f8adf396f04676d52ba061570db08aaa1d8f5078
ferv <fernandovillegass000@gmail.com>
2025-04-23 09:30:57 -0500

    proyect initialization

---
+++ src/main/java/ferv/dev/traceabilitymicroservice/TraceabilityMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ferv.dev.traceabilitymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceabilityMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceabilityMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ferv/dev/traceabilitymicroservice/TraceabilitymicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ferv.dev.traceabilitymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilitymicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
